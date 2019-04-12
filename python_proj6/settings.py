93efb0d61926791ebfe5ca513c184c62120c0fc7
Trace Harris <traceh@whiletrace.com>
2019-04-12 00:58:05 -0700

    corrected error in requirements.txt commit includes updates to migration files

--- python_proj6/settings.py
+++ python_proj6/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+STATIC_ROOT = (
     os.path.join(BASE_DIR, 'assets'),
 )
+

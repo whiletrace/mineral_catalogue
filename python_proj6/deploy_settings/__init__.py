b72d3ec1ae73ddf307bf45fee8f70ca17fe98874
Trace Harris <traceh@whiletrace.com>
2019-04-13 00:27:37 -0700

    production db settings

--- python_proj6/deploy_settings/__init__.py
+++ python_proj6/deploy_settings/__init__.py
@@ -1,5 +1,7 @@
 from python_proj6.settings import *
 
+import dj_database_url
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
@@ -9,3 +11,6 @@ ALLOWED_HOSTS = [
     ]
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
